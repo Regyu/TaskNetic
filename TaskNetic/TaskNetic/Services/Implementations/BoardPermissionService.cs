5c8c7d396823410da4d695df80ddf89b5bcd301c
SzczuruPL <szczurekpolska@gmail.com>
2024-11-29 15:36:34 +0100

    Added BoardPermission service

---
+++ TaskNetic/TaskNetic/Services/Implementations/BoardPermissionService.cs
@@ -0,0 +1,13 @@
+﻿using TaskNetic.Data.Repository;
+using TaskNetic.Data;
+using TaskNetic.Models;
+using TaskNetic.Services.Interfaces;
+
+namespace TaskNetic.Services.Implementations
+{
+    public class BoardPermissionService : Repository<BoardPermission>, IBoardPermissionService
+    {
+        public BoardPermissionService(ApplicationDbContext context) : base(context) { }
+
+    }
+}
